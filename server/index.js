464233dc042a143b1ec20b0b99426a29fcf5484f
AtlasWongy <atlasyjyj@gmail.com>
2023-02-12 22:43:38 +0800

    Revert "Revert "init server files""
    This reverts commit 8847f4aceddb8a23d9f82e88e56fcdb808d2a80b.

---
+++ server/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const connection = require("./connection")
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+module.exports = app;
